dd21a94a1a22a953e0b2fa9001d043cb15db8cfc
Julien Pivotto <roidelapluie@inuits.eu>
2011-11-08 20:33:39 +0100

    improve admin site

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 class ObjectsAdmin(admin.ModelAdmin):
-    list_display = ('common_name', 'product_name', 'location',
+    list_display = ('common_name', 'product_name', 'manufacturer', 'location',
     'reference_person', 'out_of_accreditation')
     list_filter = ( 'out_of_accreditation', 'reference_person' )
     fieldsets = [
@@ -15,10 +15,10 @@ class ObjectsAdmin(admin.ModelAdmin):
             'precautions']}),
         (_('Position'), {'fields': ['location', 'user_guide_location']}),
         (_('Maintenance'), {'fields': ['maintenance_days', 'maintenance_text',
-            'maintenance_date', 'last_maintenance_date', 'control_days',
-            'control_text', 'control_date', 'last_control_date',
+            'maintenance_date', 'next_maintenance_date', 'control_days',
+            'control_text', 'control_date', 'next_control_date',
             'calibration_days', 'calibration_text', 'calibration_date',
-            'last_calibration_date', 'overlap_days'], 'classes': ['collapse']}),
+            'next_calibration_date', 'overlap_days'], 'classes': ['collapse']}),
         (_('Assets'), {'fields': ['assets'], 'classes': ['collapse']}),
         (_('Purchase'), {'fields': ['purchase_date', 'bought_new', 'price',
             'order_date', 'activation_date', 'guarantee', 'end_date',
@@ -30,9 +30,19 @@ class ObjectsAdmin(admin.ModelAdmin):
 
     ]
 
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'zip_code', 'country')
+    list_filter = ['country']
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city')
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('city', 'address')
+
 admin.site.register(Country)
 admin.site.register(Location)
-admin.site.register(Manufacturer)
+admin.site.register(Manufacturer, ManufacturerAdmin)
 admin.site.register(Object, ObjectsAdmin)
-admin.site.register(City)
-admin.site.register(Site)
+admin.site.register(City, CityAdmin)
+admin.site.register(Site, SiteAdmin)
