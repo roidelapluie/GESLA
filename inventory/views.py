db06f7f02c46ef555842075131481367aaceb7fc
Julien Pivotto <roidelapluie@inuits.eu>
2011-11-08 20:34:39 +0100

    add views for manufacturer

--- inventory/views.py
+++ inventory/views.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from models import Object
+from models import Object, Manufacturer
 
 def index(request):
     objects = Object.objects.filter(reference_person=request.user,
@@ -23,12 +23,25 @@ def all(request):
         'closed_objects': closed_objects },
         context_instance=RequestContext(request))
 
+def manufacturer_all(request):
+    manufacturers = Manufacturer.objects.all()
+    return render_to_response('gesla/manufacturers.html', { 'manufacturers': \
+        manufacturers }, context_instance=RequestContext(request))
+
+def manufacturer_view(request, manufacturer_id):
+    manufacturer = \
+        Manufacturer.objects.get(id=manufacturer_id)
+    objects = Object.objects.filter(manufacturer=manufacturer)
+    return render_to_response('gesla/manufacturer.html', { 'objects': \
+        objects, 'manufacturer': manufacturer },
+        context_instance=RequestContext(request))
+
 def object_view(request, object_id):
     selected_object = \
         Object.objects.get(id=object_id)
-    if selected_object.reference_person != request.user:
+    if selected_object.reference_person != request.user and \
+        not request.user.is_staff:
         selected_object = None
-#        filter(reference_person=request.user)
     return render_to_response('gesla/object.html', { 'object': \
         selected_object }, context_instance=RequestContext(request))
 
